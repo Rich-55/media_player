11f0ef62e4e627e1f181606c810ec5716f64ba92
Rich-55 <thanhgiau55@gmail.com>
2025-01-10 08:45:21 +0700

    add MetadataManager

--- PlayerMediaApp/include/Model/MetadataManager.h
+++ PlayerMediaApp/include/Model/MetadataManager.h
@@ -5,15 +5,15 @@
 #include <memory>
 #include "MediaFile.h"
 
-// class MetadataManager
-//     private:
-//         std::vector<shared_ptr<MediaFile>> listMediaFiles;
+class MetadataManager
+    private:
+        std::vector<shared_ptr<MediaFile>> listMediaFiles;
 
-//     public: 
-//         void addMediaFileFromUSB(shared_ptr<MediaFile>);
-//         void addMediaFileFromPC(shared_ptr<MediaFile>);
-//         void editMediaFile(shared_ptr<MediaFile>);
-//         void removeMediaFile(shared_ptr<MediaFile>);
-//         void getAllMediaFiles() const;
+    public: 
+        void addMediaFileFromUSB(shared_ptr<MediaFile>);
+        void addMediaFileFromPC(shared_ptr<MediaFile>);
+        void editMediaFile(shared_ptr<MediaFile>);
+        void removeMediaFile(shared_ptr<MediaFile>);
+        void getAllMediaFiles() const;
 
 #endif
--- PlayerMediaApp/src/Model/MetadataManager.cpp
+++ PlayerMediaApp/src/Model/MetadataManager.cpp
@@ -1,52 +1,52 @@
-// #include "../../include/Model/MetadataManager.h"
+#include "../../include/Model/MetadataManager.h"
 
-// void MetadataManager::addMediaFileFromUSB(std::shared_ptr<MediaFile> file) {
-//     listMediaFiles.push_back(file);
-//     std::cout << "Added media file from USB: " << file->filename << std::endl;
-// }
+void MetadataManager::addMediaFileFromUSB(std::shared_ptr<MediaFile> file) {
+    listMediaFiles.push_back(file);
+    std::cout << "Added media file from USB: " << file->filename << std::endl;
+}
 
-// void addMediaFileFromPC(std::shared_ptr<MediaFile> file) {
-//         listMediaFiles.push_back(file);
-//         std::cout << "Added media file from PC: " << file->filename << std::endl;
-// }
+void addMediaFileFromPC(std::shared_ptr<MediaFile> file) {
+        listMediaFiles.push_back(file);
+        std::cout << "Added media file from PC: " << file->filename << std::endl;
+}
 
-// void editMediaFile(std::shared_ptr<MediaFile> file) {
-//     auto it = std::find_if(listMediaFiles.begin(), listMediaFiles.end(),
-//         [&](const std::shared_ptr<MediaFile>& f) {
-//             return f->filename == file->filename;
-//         });
+void editMediaFile(std::shared_ptr<MediaFile> file) {
+    auto it = std::find_if(listMediaFiles.begin(), listMediaFiles.end(),
+        [&](const std::shared_ptr<MediaFile>& f) {
+            return f->filename == file->filename;
+        });
 
-//     if (it != listMediaFiles.end()) {
-//         (*it)->filename = file->filename; 
-//         (*it)->filepath = file->filepath;
-//         std::cout << "Edited media file: " << (*it)->filename << std::endl;
-//     } else {
-//         std::cout << "Media file not found for editing." << std::endl;
-//     }
-// }
+    if (it != listMediaFiles.end()) {
+        (*it)->filename = file->filename; 
+        (*it)->filepath = file->filepath;
+        std::cout << "Edited media file: " << (*it)->filename << std::endl;
+    } else {
+        std::cout << "Media file not found for editing." << std::endl;
+    }
+}
 
-// void removeMediaFile(std::shared_ptr<MediaFile> file) {
-//     auto it = std::remove_if(listMediaFiles.begin(), listMediaFiles.end(),
-//         [&](const std::shared_ptr<MediaFile>& f) {
-//             return f->filename == file->filename;
-//         });
+void removeMediaFile(std::shared_ptr<MediaFile> file) {
+    auto it = std::remove_if(listMediaFiles.begin(), listMediaFiles.end(),
+        [&](const std::shared_ptr<MediaFile>& f) {
+            return f->filename == file->filename;
+        });
 
-//     if (it != listMediaFiles.end()) {
-//         listMediaFiles.erase(it, listMediaFiles.end());
-//         std::cout << "Removed media file: " << file->filename << std::endl;
-//     } else {
-//         std::cout << "Media file not found for removal." << std::endl;
-//     }
-// }
+    if (it != listMediaFiles.end()) {
+        listMediaFiles.erase(it, listMediaFiles.end());
+        std::cout << "Removed media file: " << file->filename << std::endl;
+    } else {
+        std::cout << "Media file not found for removal." << std::endl;
+    }
+}
 
-// void getAllMediaFiles() const {
-//     std::cout << "--- All Media Files ---" << std::endl;
-//     if (listMediaFiles.empty()) {
-//         std::cout << "No media files found." << std::endl;
-//     } else {
-//         for (const auto& file : listMediaFiles) {
-//             file.getName();
-//             std::cout << "------------------------" << std::endl;
-//         }
-//     }
-// }
+void getAllMediaFiles() const {
+    std::cout << "--- All Media Files ---" << std::endl;
+    if (listMediaFiles.empty()) {
+        std::cout << "No media files found." << std::endl;
+    } else {
+        for (const auto& file : listMediaFiles) {
+            file.getName();
+            std::cout << "------------------------" << std::endl;
+        }
+    }
+}
