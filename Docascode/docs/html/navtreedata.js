b4ec20ec0a701fe5c7296579a5d3d0774c600206
GiauPVT <thanhgiau55@gmail.com>
2025-02-03 09:43:29 +0700

    Update class diagram

--- Docascode/docs/html/navtreedata.js
+++ Docascode/docs/html/navtreedata.js
@@ -24,24 +24,12 @@
 */
 var NAVTREE =
 [
-  [ "Media Player", "index.html", [
-    [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", null ],
-        [ "Functions", "functions_func.html", null ]
-      ] ]
-    ] ],
-    [ "Files", "files.html", [
-      [ "File List", "files.html", "files_dup" ]
-    ] ]
-  ] ]
+  [ "Media Player", "index.html", ]
 ];
 
 var NAVTREEINDEX =
 [
-"annotated.html"
+"index.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronization';
